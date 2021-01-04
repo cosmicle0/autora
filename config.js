86063ee8add651339f0c05a0bf126003e6c14963
cosmicice <meowice@protonmail.ch>
2021-01-04 11:05:58 +0400

    ready for release 1.0

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     "port": "4856",
     "baseUrl": "", // Do not include a protocol (http:// \ https://) or a trailing slash! Example: go.bongo.ninja
-    "adminPassword": ""
+    "dbFileName": "autora.sqlite3"
 };
