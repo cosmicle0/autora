ce53e0f6d37d381a94e249496eb635af4a33a77f
cosmicice <meowice@protonmail.ch>
2021-01-04 11:35:18 +0400

    ok, actual v1 release!

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('../db');
+const { baseUrl } = require('../config');
 
 const router = express.Router();
 
@@ -8,7 +9,7 @@ const apiRoute = require('./api');
 router.use('/api', apiRoute);
 
 router.get('/', async (req, res) => {
-    res.status(200).render(path.join(__dirname, '../public/index.html'));
+    res.status(200).render(path.join(__dirname, '../public/index.html'), {base:baseUrl});
 });
 
 router.get('/:code', async(req, res) => {
@@ -16,10 +17,7 @@ router.get('/:code', async(req, res) => {
         if (Object.values(await db.checkIfSlugExists(req.params.code))[0] === 1) {
             res.redirect(302, Object.values(await db.selectByShortId(req.params.code))[0]);
         } else {
-            res.status(404).json({
-                status: 404,
-                message: 'Invalid Short Url!'
-            });
+            res.status(404).sendFile(path.join(__dirname, '../public/404.html'), {home:baseUrl});
         }
     } catch (err) {
         console.log(err);
